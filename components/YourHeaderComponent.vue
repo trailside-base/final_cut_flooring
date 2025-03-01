bd8649c5eb9303a23fb0f7567056c5483455657e
trailside-base <trailsidebasecamp@gmail.com>
2025-03-01 12:33:10 -0700

    fix: github pages

---
+++ components/YourHeaderComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import logoImage from '~/assets/images/logo.png'
+</script>
+
+<template>
+  <img 
+    :src="`${$config.public.baseURL}/images/logo.png`" 
+    alt="Final Cut Flooring" 
+    class="h-20 md:h-32 w-auto object-contain min-h-[5rem] md:min-h-[8rem]"
+  >
+</template> 
